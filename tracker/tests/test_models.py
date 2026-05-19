b407775a9ac67dae8fe342d0c6498f1884ce81f2
Yousef <yousef.ah@outlook.com>
2026-05-19 20:21:33 +0100

    test: add model tests for Location, Satellite, and Pass

---
+++ tracker/tests/test_models.py
@@ -0,0 +1,55 @@
+'''Unit tests for tracker models.'''
+
+import datetime
+
+from django.test import TestCase
+
+from tracker.models import Location, Satellite, Pass
+
+
+class LocationModelTest(TestCase):
+    def test_str_returns_name(self):
+        location = Location.objects.create(
+            name='North Pole',
+            latitude=45.0,
+            longitude=90,
+            altitude_m=100.0
+        )
+        self.assertEqual(str(location), 'North Pole')
+
+
+class SatelliteModelTest(TestCase):
+    def test_str_returns_name(self):
+        satellite = Satellite.objects.create(
+            name='PACE',
+            norad_id=12345,
+            satellite_type='Observation',
+            description='Engineered to explore the Universe.'
+        )
+        self.assertEqual(str(satellite), 'PACE')
+
+
+class PassModelTest(TestCase):
+    def test_str_returns_expected_string(self):
+        satellite = Satellite.objects.create(
+            name='PACE',
+            norad_id=12345,
+            satellite_type='Observation',
+            description='Engineered to explore the Universe.'
+        )
+        location = Location.objects.create(
+            name='South Pole',
+            latitude=45,
+            longitude=90,
+            altitude_m=100.0
+        )
+        dt = datetime.datetime(1990, 8, 1, 5, 8, 0, tzinfo=datetime.timezone.utc)
+        pass_event = Pass.objects.create(
+            satellite=satellite,
+            location=location,
+            datetime = dt,
+            duration_seconds = 60,
+            max_elevation_deg = 90,
+            notes = 'PACE passed over the South Pole, astonishing the wolves below.'
+        )
+        self.assertEqual(str(pass_event), f'PACE over South Pole on {dt}')
