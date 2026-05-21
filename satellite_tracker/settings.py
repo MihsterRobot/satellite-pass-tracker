afd67c15fc1cf80e083b651c530dbc5bba2433ca
Yousef <yousef.ah@outlook.com>
2026-05-21 12:32:05 +0100

    feat: add rate limiting for authenticated and anonymous users

--- satellite_tracker/settings.py
+++ satellite_tracker/settings.py
@@ -142,4 +142,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+    'rest_framework.throttling.AnonRateThrottle',
+    'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/hour',
+        'user': '100/hour',
+    }
 }
